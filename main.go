fa0b96272533ecd5ff7d8f941405b2e5a39a0edf
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-12 21:43:50 -0700

    Add hit counter

--- main.go
+++ main.go
@@ -1,20 +1,56 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
+type apiConfig struct {
+	fileServerHits atomic.Int32
+}
+
 func main() {
+	apiCfg := apiConfig{}
+
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	fileServer := http.FileServer(http.Dir("."))
+
+	mux.Handle("/app/", apiCfg.middlewareMetricsInc(http.StripPrefix("/app", fileServer)))
+	mux.HandleFunc("/healthz", healthzHandler)
+	mux.HandleFunc("/metrics", apiCfg.hitCountHandler)
+	mux.HandleFunc("/reset", apiCfg.resetHitCountHandler)
 
 	port := "8080"
-
 	server := http.Server{
 		Addr:    ":" + port,
 		Handler: mux,
 	}
 
 	log.Fatal(server.ListenAndServe())
+}
+
+func healthzHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileServerHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
+func (cfg *apiConfig) hitCountHandler(w http.ResponseWriter, r *http.Request) {
+	hitCount := cfg.fileServerHits.Load()
+	message := fmt.Sprintf("Hits: %d", hitCount)
+	w.Write([]byte(message))
+}
+
+func (cfg *apiConfig) resetHitCountHandler(w http.ResponseWriter, r *http.Request) {
+	cfg.fileServerHits.Store(int32(0))
+	w.Write([]byte("Hit count successfully reset"))
 }
