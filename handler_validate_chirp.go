b2742591631d923fcabc5850600fde6f20d7302b
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-14 17:20:30 -0700

    Add word filter

---
+++ handler_validate_chirp.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"strings"
+)
+
+func respondWithJSON(w http.ResponseWriter, code int, payload any) {
+	data, err := json.Marshal(payload)
+	if err != nil {
+		log.Printf("error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func respondWithError(w http.ResponseWriter, code int, msg string) {
+	type errorResponse struct {
+		Error string `json:"error"`
+	}
+
+	errMsg := errorResponse{Error: msg}
+
+	data, err := json.Marshal(errMsg)
+	if err != nil {
+		log.Printf("error marshalling JSON: %s", err)
+		w.WriteHeader(500)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	w.Write(data)
+}
+
+func filterMsg(msg string) string {
+	lower := strings.ToLower(msg)
+	split := strings.Split(lower, " ")
+	originalSplit := strings.Split(msg, " ")
+	for i := range split {
+		if split[i] == "kerfuffle" || split[i] == "sharbert" || split[i] == "fornax" {
+			originalSplit[i] = "****"
+		}
+	}
+
+	return strings.Join(originalSplit, " ")
+}
+
+func validateChirpHandler(w http.ResponseWriter, r *http.Request) {
+	type parameters struct {
+		Body string `json:"body"`
+	}
+
+	type validResponse struct {
+		CleanedBody string `json:"cleaned_body"`
+	}
+
+	decoder := json.NewDecoder(r.Body)
+	params := parameters{}
+	err := decoder.Decode(&params)
+
+	if err != nil {
+		respondWithError(w, 500, "Something went wrong")
+		return
+	}
+
+	if len(params.Body) > 140 {
+		respondWithError(w, 400, "Chirp is too long")
+		return
+	}
+
+	respondWithJSON(w, http.StatusOK, validResponse{CleanedBody: filterMsg(params.Body)})
+}
