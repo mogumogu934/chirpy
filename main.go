326e89c332fe1deccda81d116d3e61fc35e5f829
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-15 00:53:27 -0700

    Make metrics naming consistent

--- main.go
+++ main.go
@@ -52,7 +52,7 @@ func main() {
 	mux.HandleFunc("POST /api/users", apiCfg.createUserHandler)
 	mux.HandleFunc("POST /api/validate_chirp", validateChirpHandler)
 	mux.HandleFunc("GET /admin/metrics", apiCfg.metricsHandler)
-	mux.HandleFunc("POST /admin/reset-hits", apiCfg.resetMetricsHandler)
+	mux.HandleFunc("POST /admin/reset-metrics", apiCfg.resetMetricsHandler)
 	mux.HandleFunc("POST /admin/reset", apiCfg.resetHandler)
 
 	port := "8080"
