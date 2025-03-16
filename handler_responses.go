413f82b4fe3c21f59f19970a0e7bcc2268e7e1c4
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-15 20:58:05 -0700

    Use recommended format specifier for errors

--- handler_responses.go
+++ handler_responses.go
@@ -9,7 +9,7 @@ import (
 func respondWithJSON(w http.ResponseWriter, code int, payload any) {
 	data, err := json.Marshal(payload)
 	if err != nil {
-		log.Printf("error marshalling JSON: %s", err)
+		log.Printf("error marshalling JSON: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
@@ -28,7 +28,7 @@ func respondWithError(w http.ResponseWriter, code int, msg string) {
 
 	data, err := json.Marshal(errMsg)
 	if err != nil {
-		log.Printf("error marshalling JSON: %s", err)
+		log.Printf("error marshalling JSON: %v", err)
 		w.WriteHeader(http.StatusInternalServerError)
 		return
 	}
