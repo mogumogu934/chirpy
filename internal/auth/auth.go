0661eabbd05668d489937d392d45ccd3abfe2052
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-16 15:24:38 -0700

    Change argument order according to specifications

--- internal/auth/auth.go
+++ internal/auth/auth.go
@@ -15,6 +15,6 @@ func HashPassword(password string) (string, error) {
 	return string(hashedPassword), nil
 }
 
-func CheckPasswordHash(hash, password string) error {
+func CheckPasswordHash(password, hash string) error {
 	return bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
 }
