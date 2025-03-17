cf6c0e72f307a3602ef4f77b7bee8423afa3176a
mogumogu934 <107589445+mogumogu934@users.noreply.github.com>
2025-03-16 17:42:34 -0700

    Check password field for emptiness.

--- handler_login.go
+++ handler_login.go
@@ -4,45 +4,34 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
-	"time"
 
-	"github.com/google/uuid"
 	"github.com/mogumogu934/chirpy/internal/auth"
 )
 
 func (cfg *apiConfig) loginHandler(w http.ResponseWriter, r *http.Request) {
-	type cleanUser struct {
-		ID        uuid.UUID
-		CreatedAt time.Time
-		UpdatedAt time.Time
-		Email     string
-	}
-
 	decoder := json.NewDecoder(r.Body)
-	params := parameters{}
+	params := userReq{}
 	err := decoder.Decode(&params)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, fmt.Sprintf("error decoding body: %v", err))
 		return
 	}
 
-	user, err := cfg.db.GetUserByEmail(r.Context(), params.Email)
+	if params.Password == "" {
+		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password")
+		return
+	}
+
+	dbUser, err := cfg.db.GetUserByEmail(r.Context(), params.Email)
 	if err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password")
 		return
 	}
 
-	if err := auth.CheckPasswordHash(params.Password, user.HashedPassword); err != nil {
+	if err := auth.CheckPasswordHash(params.Password, dbUser.HashedPassword); err != nil {
 		respondWithError(w, http.StatusUnauthorized, "Incorrect email or password")
 		return
 	}
 
-	loggedInUser := cleanUser{
-		ID:        user.ID,
-		CreatedAt: user.CreatedAt,
-		UpdatedAt: user.UpdatedAt,
-		Email:     user.Email,
-	}
-
-	respondWithJSON(w, http.StatusOK, loggedInUser)
+	respondWithJSON(w, http.StatusOK, cleanUserResp(dbUser))
 }
